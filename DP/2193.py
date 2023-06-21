cd8fdc37a725b308e006dccc247b59e555e8313c
youngseok.jang <ys123.jang@gmail.com>
2023-06-21 10:13:37 +0900

    DP + 3

---
+++ DP/2193.py
@@ -0,0 +1,17 @@
+# 2193
+# Silver III
+
+N = int(input())
+
+dp = [[0] * 2 for _ in range(N + 1)]
+
+dp[1] = [0, 1]
+
+for i in range(2, N + 1):
+    dp[i][0] = dp[i-1][1] + dp[i-1][0]
+    dp[i][1] = dp[i-1][0]
+
+
+
+
+print(sum(dp[N]))
